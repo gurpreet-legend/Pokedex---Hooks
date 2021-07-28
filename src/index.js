7b06174e6947dbb9b40bec4aa75f09b87e29cf0d
Gurpreet <lostlegend2701@gmail.com>
2021-07-28 23:48:56 +0530

    created store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'react';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './containers/App.js';
+import {searchRobots} from './reducers'
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-  <App />,
+  <Provider store= {store}>
+    <App />
+  </Provider>, 
   document.getElementById('root')
 );
 
